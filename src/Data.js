6bf1a1b760d17422ca2c58da354a4d34865bacc2
cyanchill <anthonyliang9@gmail.com>
2021-10-11 12:07:36 -0400

    basic state structure for cards

---
+++ src/Data.js
@@ -0,0 +1,100 @@
+const defaultCardData = [
+  {
+    id: 1,
+    clicked: false,
+    cardInfo: {
+      title: "",
+      image: "",
+    },
+  },
+  {
+    id: 2,
+    clicked: false,
+    cardInfo: {
+      title: "",
+      image: "",
+    },
+  },
+  {
+    id: 3,
+    clicked: false,
+    cardInfo: {
+      title: "",
+      image: "",
+    },
+  },
+  {
+    id: 4,
+    clicked: false,
+    cardInfo: {
+      title: "",
+      image: "",
+    },
+  },
+  {
+    id: 5,
+    clicked: false,
+    cardInfo: {
+      title: "",
+      image: "",
+    },
+  },
+  {
+    id: 6,
+    clicked: false,
+    cardInfo: {
+      title: "",
+      image: "",
+    },
+  },
+  {
+    id: 7,
+    clicked: false,
+    cardInfo: {
+      title: "",
+      image: "",
+    },
+  },
+  {
+    id: 8,
+    clicked: false,
+    cardInfo: {
+      title: "",
+      image: "",
+    },
+  },
+  {
+    id: 9,
+    clicked: false,
+    cardInfo: {
+      title: "",
+      image: "",
+    },
+  },
+  {
+    id: 10,
+    clicked: false,
+    cardInfo: {
+      title: "",
+      image: "",
+    },
+  },
+  {
+    id: 11,
+    clicked: false,
+    cardInfo: {
+      title: "",
+      image: "",
+    },
+  },
+  {
+    id: 12,
+    clicked: false,
+    cardInfo: {
+      title: "",
+      image: "",
+    },
+  },
+];
+
+export { defaultCardData };
