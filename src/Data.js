1fca60191d118439c79d9f90b7e61816a1a73401
cyanchill <anthonyliang9@gmail.com>
2021-10-11 13:41:21 -0400

    add image and title information for cards

--- src/Data.js
+++ src/Data.js
@@ -3,96 +3,96 @@ const defaultCardData = [
     id: 1,
     clicked: false,
     cardInfo: {
-      title: "",
-      image: "",
+      title: "Bulbasaur",
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png",
     },
   },
   {
     id: 2,
     clicked: false,
     cardInfo: {
-      title: "",
-      image: "",
+      title: "Ivysaur",
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png",
     },
   },
   {
     id: 3,
     clicked: false,
     cardInfo: {
-      title: "",
-      image: "",
+      title: "Venusaur",
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png",
     },
   },
   {
     id: 4,
     clicked: false,
     cardInfo: {
-      title: "",
-      image: "",
+      title: "Charmander",
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png",
     },
   },
   {
     id: 5,
     clicked: false,
     cardInfo: {
-      title: "",
-      image: "",
+      title: "Charmeleon",
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/005.png",
     },
   },
   {
     id: 6,
     clicked: false,
     cardInfo: {
-      title: "",
-      image: "",
+      title: "Charizard",
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png",
     },
   },
   {
     id: 7,
     clicked: false,
     cardInfo: {
-      title: "",
-      image: "",
+      title: "Squirtle",
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png",
     },
   },
   {
     id: 8,
     clicked: false,
     cardInfo: {
-      title: "",
-      image: "",
+      title: "Wartortle",
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/008.png",
     },
   },
   {
     id: 9,
     clicked: false,
     cardInfo: {
-      title: "",
-      image: "",
+      title: "Blastoise",
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/009.png",
     },
   },
   {
     id: 10,
     clicked: false,
     cardInfo: {
-      title: "",
-      image: "",
+      title: "Caterpie",
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/010.png",
     },
   },
   {
     id: 11,
     clicked: false,
     cardInfo: {
-      title: "",
-      image: "",
+      title: "Metapod",
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/011.png",
     },
   },
   {
     id: 12,
     clicked: false,
     cardInfo: {
-      title: "",
-      image: "",
+      title: "Butterfree",
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/012.png",
     },
   },
 ];
